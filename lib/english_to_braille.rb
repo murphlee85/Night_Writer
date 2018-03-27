5e06fa92399ebbb2504ea4777d5158f721d5fca0
mellisaluce <mjlgluce@gmail.com>
2018-03-27 17:58:25 -0600

    Update Test File Psuedocode

--- lib/english_to_braille.rb
+++ lib/english_to_braille.rb
@@ -1,4 +1,5 @@
-require 'pry'
+class NightWriter
+end 
 
 
 #iterate through text
--- test/night_writer_test.rb
+++ test/night_writer_test.rb
@@ -4,4 +4,24 @@ require './lib/english_to_braille'
 
 
 class NightWriterTest < Minitest::Test
+
+  def test_night_writer_exists
+    night_writer = NightWriter.new
+
+    assert_instance_of NightWriter, night_writer
+  end
+
+  def test_can_encode_to_braille
+  end
+
+  def test_can_decode_to_english
+  end
+
+  def test_has_input
+  end
+
+  def test_has_output
+  end 
+
+
 end
